6251b059de540277bcce5eb56f305051f9d04275
Farman Ali <138219232+farman72@users.noreply.github.com>
2024-09-13 18:24:37 +0500

    files added

---
+++ chapters01-06/app.js
@@ -0,0 +1,353 @@
+// var num1 = 12;
+
+// var num2 = 10;
+
+// var result = num1 + num2;
+
+// alert(result);
+// alert("Hello")
+
+// chapter1------------>>>>
+
+// Q1
+// alert("Hello")
+
+// Q2
+// alert("Error! Please enter a valid password")
+
+// Q3
+// alert("Wellcome to js land\n Happy coding!")
+
+// Q4
+// alert("Wellcome to js land...")
+
+// Q5
+// var abc = "Hello...I can run js thorough my web browser's console";
+// console.log(abc)
+
+// Q6
+
+
+
+// chapter2------------>>>>
+// Q1
+// var username
+
+// Q2
+// var myname = "Farman ali"
+// alert(myname)
+
+// Q3
+// var message = "Hello World"
+// alert(message)
+
+// Q4
+// var myname = "farman"
+// var age = "24"
+// var course = "web & app development from smit bacht 12"
+
+// alert(myname)
+// alert(age)
+// alert(course)
+
+
+// Q5
+// alert("PIZA\n PIZZ\n PIZ\n PI\n P")
+// // alert(message);
+
+// Q6
+// var email = "My email addres is \n fa.farman72@gmail.com"
+// alert(email)
+
+// Q7
+// var book = "A smarter way to learn javascript"
+// var book1 = "I am trying to learn"
+
+// alert(book1 + book)
+
+// Q8
+// document.write("Yeh!I can write HTML content through JavaScript")
+
+// Q9
+// var name1 = "▀▄▀▄▀▄🅈🄾🅄🅁 🄽🄰🄼🄴▀▄▀▄▀▄"
+// alert(name1)
+
+// chapter3------------>>>>
+
+
+
+// Q1
+// var age = "I am 24 years old"
+// alert(age);
+
+// Q3
+// var myage = "30"
+// console.log(myage)
+// Q2
+// var cbc = 1
+// alert("you have visted this site " + cbc + " times")
+// var cbc = cbc + 1
+// alert("you have visted this site " + cbc + " times")
+// var cbc = cbc + 1
+// alert("you have visted this site " + cbc + " times")
+// var cbc = cbc + 1
+// alert("you have visted this site " + cbc + " times")
+// var cbc = cbc + 1
+// alert("you have visted this site " + cbc + " times")
+// var cbc = cbc + 1
+// alert("you have visted this site " + cbc + " times")
+// var cbc = cbc + 1
+// alert("you have visted this site " + cbc + " times")
+// var cbc = cbc + 1
+// alert("you have visted this site " + cbc + " times")
+
+// var num = 1;
+// alert(num)
+
+// num = num + 1;
+// alert(num)
+
+// num = num + 1;
+// alert(num)
+
+// num = num + 1;
+// alert(num)
+
+// num = num + 1;
+// alert(num)
+
+// var num = 1;
+// alert(num)
+
+// num = num + 1;
+// alert(num) 
+
+// num = num + 1;
+// alert(num)
+
+
+
+// var num = 8 - 7 + 2 * (6 /2) + 9;
+// alert(num) 
+
+
+// chapter 04 ------------>>>>
+
+//  Assignment # 4
+//  VARIABLE NAMES: LEGAL & ILLEGAL
+
+
+// // Q1. Declare 3 variables in one statement
+// var x = 1, y = 2, z = "hello";
+// alert(x)
+// alert(y)
+// alert(z)
+
+// // Q2. Declare 5 legal & 5 illegal variable 
+
+// // Legal variable names
+// var myVar;
+// var $myVar;
+// var _myVar;
+// var myVar1;
+// var my_Var;
+
+// // Illegal variable names
+// // var 1myVar; // Starts with a number
+// // var my-Var; // Contains a hyphen
+// // var my Var; // Contains a space
+// // var myVar&; // Contains an invalid character
+// // var myVar!; // Contains an invalid character
+
+// // Q3. Display the rules in the browser
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("<p>Variable names can only contain letters, numbers, underscores, and dollar signs. For example $my_1stVariable</p>");
+// document.write("<p>Variables must begin with a letter, underscore, or dollar sign. For example $name, _name, or name</p>");
+// document.write("<p>Variable names are case sensitive</p>");
+// document.write("<p>Variable names should not be JS keywords</p>");
+
+
+
+//  Assignment #5
+
+
+// //  MATH EXPRESSIONS 
+// Q1
+
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1 + num2;
+// var sub = num2 - num1;
+// var multi = num2 * num1;
+// var divi = num2 / num1;
+// var modulus = num1 % num2;
+
+// document.write("Sum of " + num1 + " and " + num2 + " is " + sum + "<br><br>");
+// document.write("Sub of " + num1 + " and " + num2 + " is " + sub + "<br><br>");
+// document.write("Multiplication of " + num1 + " and " + num2 + " is " + multi + "<br><br>");
+// document.write("Division of " + num1 + " and " + num2 + " is " + divi + "<br><br>");
+// document.write("Modulus of " + num1 + " and " + num2 + " is " + modulus + "<br><br>");
+
+
+
+
+// Q2
+
+// var empty ;
+// var initial = 5;
+
+// document.write("Value after variable declaration is: " + empty + "<br><br>");
+// document.write("Initial value is: " + initial + "<br><br>");
+// initial++;
+// document.write("Initial value after increment: " + initial + "<br><br>");
+// initial = initial + 7
+// document.write("Initial value after addition is: " + initial + "<br><br>");
+// initial--
+// document.write("Initial value after decrement is: " + initial + "<br><br>");
+// initial = initial % 2;
+// console.log(initial);
+
+// document.write("The remainder is: " + initial + "<br><br>");
+
+// ----------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------
+
+// Q3.
+
+// var tickets = 600;
+// var userInput = prompt("How many tickets your are buying")
+// var numberofticket = (userInput)
+
+// document.write("Total cost to buy "+ numberofticket + "  tickets to a movie is " + tickets*userInput + "PKR" )
+
+
+
+
+
+// Q4.
+
+
+// var tableOf = prompt("Table of") ;
+
+
+// document.write("<h1>"+"Table of ", tableOf +"</h1>" + "<br><br>")
+// document.write( tableOf + " x " + 1 + " =  " + (tableOf*1) + "<br></br>" )
+// document.write( tableOf + " x " + 1 + " =  " + (tableOf*2) + "<br></br>" )
+// document.write( tableOf + " x " + 1 + " =  " + (tableOf*3) + "<br></br>" )
+// document.write( tableOf + " x " + 1 + " =  " + (tableOf*4) + "<br></br>" )
+// document.write( tableOf + " x " + 1 + " =  " + (tableOf*5) + "<br></br>" )
+// document.write( tableOf + " x " + 1 + " =  " + (tableOf*6) + "<br></br>" )
+// document.write( tableOf + " x " + 1 + " =  " + (tableOf*7) + "<br></br>" )
+// document.write( tableOf + " x " + 1 + " =  " + (tableOf*8) + "<br></br>" )
+// document.write( tableOf + " x " + 1 + " =  " + (tableOf*9) + "<br></br>" )
+// document.write( tableOf + " x " + 1 + " =  " + (tableOf*10) + "<br></br>" )
+
+
+
+// Q5.
+
+
+// var cen = prompt("Centegrade to Fahrenheit");
+// var fan = prompt("Fahrenheit to Centegrade ");
+
+// var cenFormula = (fan - 32) * 5 / 9;
+// var fanFormula = (cen * 9 / 5) + 32;
+
+// document.write( cen + " &#8451 "+ " is " + fanFormula + "&#8457" +"<br></br>")
+// document.write( fan + " &#8457 "+ " is " + cenFormula + "&#8451")
+
+
+
+
+// Q6.
+
+// alert("we are seling only tomato and potato");
+// var potatoKg = prompt("How many kg potato you want");
+// var tomatoKg = prompt("How many kg tomato you want");
+// var potato = 100;
+// var tomato = 150;
+// var shiping = 100;
+// var totalCost = potatoKg*potato+tomato*tomatoKg+shiping;
+
+// document.write("Price of Potato: " + potato+"<br></br>");
+// document.write("quantity of Potato: " + potatoKg+"KG"+"<br></br>");
+// document.write("Price of Tomato: " + tomato +"<br></br>");
+// document.write("quantity of tomtato: " + tomatoKg+"KG"+"<br></br>");
+// document.write("Shiping charges: " + shiping +"<br></br>")
+// document.write("Total cost of your order is: " + totalCost)
+
+
+
+
+
+
+// Q7.
+
+// var totalMarks = prompt("Enter Total Marks");
+// var obtainMarks = prompt("Enter obtained Marks");
+// var percentage = obtainMarks / totalMarks * 100;
+// document.write("<h1>Mark Sheet</h1>" + "<br></br>");
+// document.write("<h1>" +"Total Marks: ",totalMarks +"</h1>" + "<br></br>");
+// document.write("<h1>" +"Obtained Marks: ",obtainMarks +"</h1>" + "<br></br>");
+// document.write("<h1>" +"Percentage: ",percentage +"</h1>" + "<br></br>");
+
+
+
+
+// Q8.
+
+// var converter = prompt("USD to PKR");
+// var usd = 1;
+// var pkr = 277.45;
+// var convert = converter*pkr;
+// document.write("<h1>Currency in PKR</h1>" + "<br></br>");
+// document.write("Total currency in PKR: " + convert);
+
+
+
+
+// var singleExpression = (5*10)/5
+// alert(singleExpression)
+
+
+
+
+// Q9.
+
+// var birthYear = prompt("Enter birth year!");
+// var currentYear = 2024;
+// var age = currentYear-birthYear;
+
+// document.write("<h1>Age calculator:</h1> " + "<br></br>");
+// document.write("Current Year: " + currentYear+ "<br></br>");
+// document.write("Birth Year: " + birthYear+ "<br></br>");
+// document.write("Your Age: " + age);
+
+
+
+
+// //  Assignment #6
+
+// //  MATH EXPRESSIONS 
+// //  Start 
+
+// Q1.
+
+// var a = 10;
+// a = ++a;
+// document.write("The value of ++a is :" + a + "<br>")
+// document.write("Now the value of a is :" + a + "<br><br>")
+
+
+// document.write("The value of a++ is :" + a + "<br>")
+// a++;
+// document.write("Now the value of a is :" + a + "<br><br>")
+
+// --a;
+// document.write("The value of --a is :" + a + "<br>")
+// document.write("Now the value of a is :" + a + "<br><br>")
+
+// document.write("The value of a-- is :" + a + "<br>")
+// a--;
+// document.write("Now the value of a is :" + a + "<br><br>")
+
